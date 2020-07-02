876f6eb2cff7be3efc8eccb921457ac9e45cf226
Seán Healy <sean.healy@visma.com>
2020-07-02 20:23:28 +0100

    Update install script.

--- ubuntu-automation/install.sh
+++ ubuntu-automation/install.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/bash
-(sudo git clone https://github.com/sean-healy/newsreduce /opt/newsreduce && cd /opt/newsreduce/ubuntu-automation && ./install-main.sh)
+if [ -d /opt/newsreduce ]; then
+    (cd /opt/newsreduce && git pull && cd ubuntu-automation && ./install-main.sh)
+else
+    (cd /opt && sudo git clone https://github.com/sean-healy/newsreduce && cd newsreduce/ubuntu-automation && ./install-main.sh)
+fi
