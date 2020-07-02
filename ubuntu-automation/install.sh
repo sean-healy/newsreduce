ca4c2e35b65e4c3bf167875a105aed072fc36476
Seán Healy <sean.healy@visma.com>
2020-07-02 20:25:36 +0100

    Fix sudo

--- ubuntu-automation/install.sh
+++ ubuntu-automation/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 if [ -d /opt/newsreduce ]; then
-    (cd /opt/newsreduce && git pull && cd ubuntu-automation && ./install-main.sh)
+    (cd /opt/newsreduce && sudo git pull && cd ubuntu-automation && ./install-main.sh)
 else
     (cd /opt && sudo git clone https://github.com/sean-healy/newsreduce && cd newsreduce/ubuntu-automation && ./install-main.sh)
 fi
