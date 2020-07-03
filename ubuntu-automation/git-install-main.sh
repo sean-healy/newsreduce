1eb79fa767fa777526c6bbefe246668283e33577
Seán Healy <sean@seanh.sh>
2020-07-03 09:39:33 +0100

    Add separate install for worker.

---
+++ ubuntu-automation/git-install-main.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/bash
+#function mysql() {
+#    cat
+#}
+#function iptables-restore() {
+#    cat
+#}
+if [ $USER != root ]; then
+    sudo $0 $@
+    exit
+fi
+bash "$(dirname $0)/install-common.sh"
+debs=(
+    mysql-server
+)
+apt-get install ${debs[*]}
+if [ ! -f /var/newsreduce/network ] || [ ! "$(cat /var/newsreduce/network)" ]; then
+    echo 'Replace file contents with a list of hosts in the newsreduce network.'\
+        > /var/newsreduce/network
+fi
+vi /var/newsreduce/network
+sql_password="$(dd if=/dev/urandom bs=15 count=1 2>/dev/null | base64 | tr '/' '_' | tr '+' ',')"
+echo "$sql_password" > /var/newsreduce/sql_password
+    echo Safely creating user...
+echo "drop user if exists newsreduce;"                                                                                            | mysql
+echo "drop user if exists 'newsreduce'@'localhost';"                                                                              | mysql
+echo "drop user if exists 'newsreduce'@'%';"                                                                                      | mysql
+    echo Creating user...
+echo "create user if not exists 'newsreduce'@'%' identified with mysql_native_password by '$sql_password';"                       | mysql
+    echo Creating DB...
+echo "create database if not exists newsreduce;"                                                                                  | mysql
+    echo Grant privileges...
+echo "grant all privileges on newsreduce.* to 'newsreduce'@'%';flush privileges;"                                                 | mysql
+echo "set global sql_mode='STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';"  | mysql
+echo "set session sql_mode='STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';" | mysql
+
+chown -R newsreduce:newsreduce /var/newsreduce
---
+++ ubuntu-automation/git-install-worker.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+if [ $USER != root ]; then
+    sudo $0 $@
+    exit
+fi
+access_key="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDDSt1FcTlxRgABMz/qyTbe56PEF8K+voO+t+zLO286XjmNscsydjWCRD4R9CDllZVOuDEiBdQtFK+If9TBFfHi3s2uS/CgpeoluoRd2MGwT96d6GWxOnxL04fWKeEJWrrePBhPqqheB1vmjr6x20bmJCBSyxxjMVYCOmCgytUoiT8zCd6SuIuuZdUUjfg2jspGUw/yFXXr2qyUVtwklaf5Ly+KhfRoHMe83SXFUp88jjM3nxK6i8VMQ9HYn5UGAMnJC2IzL7imIdxBs6u21eJerJmKoK1hTleL+Izt7ptrwc+pmesxBM7JqxXvMgZ6tUWDE9sG23SLbCwZYYY6ip33O6r3qjDIl/xK2tNWeMrBDEPeIFqkkGguaEiQWFUvQYxJ+DzJOk95TViVQNVpWLPfQf1b+taFq9Fgp1B6+u8oodWRwkEyYqW7Irnjmp/pbVaxmborXxTg+cOpQ6d0DXzFPp67uofsWniGF8OxjS3taELCeuklhOKtCS2eANf3prM= sean@allen"
+mkdir -p "/var/newsreduce/.ssh"
+ssh_authorized_keys="$ssh_dir/authorized_keys"
+if [ -f "$ssh_authorized_keys" ]; then
+    (echo "$access_key" && cat "$ssh_authorized_keys")\
+        | sort -u > "$ssh_authorized_keys.tmp"
+    mv "$ssh_authorized_keys.tmp" "$ssh_authorized_keys"
+else
+    echo "$access_key" > "$ssh_authorized_keys"
+fi
+debs=(
+    build-essential
+    checkinstall
+    libssl-dev
+    redis
+    npm
+)
+apt-get update
+apt-get -y install ${debs[*]}
+npm i -g npm
