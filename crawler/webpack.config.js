b0435ad55b3b843404e8fb5d57c2518c7dbf1dcf
Seán Healy <sean@seanh.sh>
2020-07-07 17:35:53 +0200

    Miscellaneous.

--- crawler/webpack.config.js
+++ crawler/webpack.config.js
@@ -8,6 +8,8 @@ module.exports = {
       'fetch-zookeeper': './dist/services/fetcher/zookeeper.js',
       'html-process':    './dist/html-processor/process.js',
       'schedule':        './dist/services/scheduler.js',
+      'main-net':        './dist/services/net-agent/for-main.js',
+      'worker-net':      './dist/services/net-agent/for-worker.js',
   },
   externals: [
       "jsdom",
