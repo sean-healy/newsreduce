63589c1cb864bf031a46f9e1c53b631f1a7535ea
Seán Healy <sean.healy@visma.com>
2020-07-14 15:39:55 +0200

    Add changes.

--- crawler/src/main/services/net-agent/for-worker.ts
+++ crawler/src/main/services/net-agent/for-worker.ts
@@ -6,6 +6,7 @@ async function watch() {
     const ip = await myIP();
     const client = newRedis(ip);
     client.subscribe(NET_AGENT_LOG);
+    console.log("Subscribed to channel:", NET_AGENT_LOG);
     client.on(DELETE_FILES, (_, msg: string) => {
         const lines = msg.split("\n").map(line => line.split("\t", 2));
         console.log(lines);
