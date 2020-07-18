6c8264604062b59301cf5120ad925461a9c6083d
Seán Healy <sean@seanh.sh>
2020-07-18 14:07:02 +0200

    2020-07-18

--- crawler/src/main/bash/cleanup-empty-dirs.sh
+++ crawler/src/main/bash/cleanup-empty-dirs.sh
@@ -1,11 +1,3 @@
 #!/usr/bin/bash
-# Create a buffer file.
-toRemove=/tmp/$(openssl rand -hex 30)
-# Find all directories last modified more than 30 minutes ago.
-(cd /var/newsreduce/tmp && find -type d -empty -mmin +30 -print) > $toRemove
-cat $toRemove | while read dir; do
-    # Remove those directories that were found.
-    rmdir $dir
-done
-# And remove the file used by this script.
-rm $toRemove
+# Find all directories last modified more than 30 minutes ago, and remove them.
+find /var/newsreduce/tmp -type d -empty -mmin +30 -exec rmdir {} \;
