3ba35184ef7ab0363e54ea2a8b85d96f03073980
Seán Healy <sean@seanh.sh>
2020-07-18 18:16:30 +0200

    Fix cleanup script.

--- crawler/src/main/bash/cleanup-empty-dirs.sh
+++ crawler/src/main/bash/cleanup-empty-dirs.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
 # Find all directories last modified more than 30 minutes ago, and remove them.
-find /var/newsreduce/tmp -type d -empty -mmin +30 -exec rmdir {} \;
+find /var/newsreduce/tmp -type d -empty ! -name tmp -mmin +30 -exec rmdir {} \;
+
