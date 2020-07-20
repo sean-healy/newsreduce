b65dee6031a0884f6fb172136fc1de771d1d635b
Seán Healy <sean@seanh.sh>
2020-07-20 19:01:16 +0200

    Turn off logging.

--- crawler/src/main/common/logging.ts
+++ crawler/src/main/common/logging.ts
@@ -1,9 +1,13 @@
 import fs from "fs";
 import { varDirPromise } from "./config";
 
+const TOGGLE = false;
+
 export async function log(...args: any) {
-    const now = Date.now();
-    const varDir = await varDirPromise();
-    const logFile = `${varDir}/log`;
-    fs.appendFileSync(logFile, `${now}\t${args.join(" ")}\n`);
+    if (TOGGLE) {
+        const now = Date.now();
+        const varDir = await varDirPromise();
+        const logFile = `${varDir}/log`;
+        fs.appendFileSync(logFile, `${now}\t${args.join(" ")}\n`);
+    }
 }
