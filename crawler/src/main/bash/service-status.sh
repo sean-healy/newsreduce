5a15df9cd37401dbafcf82dac13f95314ee25cec
Seán Healy <sean@seanh.sh>
2020-07-21 16:34:21 +0200

    2020-07-21: experiment

--- crawler/src/main/bash/service-status.sh
+++ crawler/src/main/bash/service-status.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 processes="$(ps -aux | grep -F service-status.sh | grep -v grep | awk '{print $1" "$5" "$7" "$8" "$11" "$12}' | uniq | wc -l)"
+echo "$processes" > /tmp/foobar
 if [ "$processes" -ge 2 ]; then
     echo "Service status already running."
     exit
