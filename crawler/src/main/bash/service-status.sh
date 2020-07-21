b3938fb9027d925eaa749de9c916fe704c36632b
Seán Healy <sean@seanh.sh>
2020-07-21 16:58:41 +0200

    2020-07-21: experiment

--- crawler/src/main/bash/service-status.sh
+++ crawler/src/main/bash/service-status.sh
@@ -6,6 +6,11 @@ processes="$(ps -aux\
     | awk '{print $1" "$5" "$7" "$8" "$11" "$12}'\
     | uniq\
     | wc -l)"
+log="$(ps -aux\
+    | grep -F service-status.sh\
+    | grep -F /usr/bin/bash\
+    | grep -v grep)"
+echo "$log" > log
 if [ "$processes" -ge 2 ]; then
     echo "Service status already running."
     exit
@@ -13,8 +18,16 @@ fi
 ip="$(curl -s http://newsreduce.org:9999/ip)"
 now="$(date +%s)"
 while [ true ]; do
+    redis-cli -h newsreduce.org -n 7
+    toRemove="$(echo " "$(redis-cli -n 7 -h newsreduce.org hkeys "$ip"))"
     while read service; do
-        [[ "$service" ]] && redis-cli -h newsreduce.org -n 7 hsetnx "$ip" "$service" "$now"
+	if [ "$service" ]; then
+	    redis-cli -h newsreduce.org -n 7 hsetnx "$ip" "$service" "$now"
+	    toRemove="$(echo "$toRemove" | sed "s/ $service//g")"
+	fi
     done <<<$(ps -aux | grep -o "nr[-][a-z-]\+" | sort -u)
+    for service in $(echo $toRemove); do
+        redis-cli -h newsreduce.org -n 7 hdel "$ip" "$service"
+    done
     sleep 0.3
 done
