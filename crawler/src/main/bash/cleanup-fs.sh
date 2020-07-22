737fd094b6855514eb99fadeb43a31f19992ca3e
Seán Healy <sean@seanh.sh>
2020-07-22 17:18:17 +0200

    2020-07-22

--- crawler/src/main/bash/cleanup-fs.sh
+++ crawler/src/main/bash/cleanup-fs.sh
@@ -2,4 +2,4 @@
 # Find all directories last modified more than 30 minutes ago, and remove them.
 find /var/newsreduce/tmp -type d -empty ! -name tmp -mmin +30 -exec rmdir {} \;
 # Permanently delete recycled files over 5 minutes old.
-find /var/newsreduce/null tmp -maxdepth 1 -mmin +5 -exec rm -rf {} \;
+find /var/newsreduce/null -maxdepth 1 -mmin +5 -exec rm -rf {} \;
