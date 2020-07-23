7af514253c17046c2fcf8cdc635f672925565900
Seán Healy <sean@seanh.sh>
2020-07-23 16:39:55 +0200

    Also delete old files from /tmp

--- crawler/src/main/bash/cleanup-fs.sh
+++ crawler/src/main/bash/cleanup-fs.sh
@@ -3,3 +3,5 @@
 find /var/newsreduce/tmp -type d -empty ! -name tmp -mmin +30 -exec rmdir {} \;
 # Permanently delete recycled files over 5 minutes old.
 find /var/newsreduce/null -maxdepth 1 -mmin +5 -exec rm -rf {} \;
+# Permanently delete tmp buffer files over an hour old.
+find /tmp -maxdepth 1 -type f -regextype egrep -regex '/tmp/[0-9a-f]{60}' -group newsreduce -mmin +60 -delete
