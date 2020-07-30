636160fe83a67fd4b7b59406223fa336352ad4ce
Seán Healy <sean@seanh.sh>
2020-07-30 12:45:17 +0100

    2020-07-30

---
+++ sql/SELECT_BAG_OF_WORDS_RESOURCE_HOST_PAIRS.sql
@@ -0,0 +1,10 @@
+select
+    u.id resource,
+    max(v.time) time,
+    u.host
+from Host h
+inner join ResourceURL u on u.host = h.id
+inner join ResourceVersion v on v.resource = u.id
+inner join ResourceVersionType t on t.id = v.type
+where t.filename = "bow.bin"
+group by v.resource, v.type;
