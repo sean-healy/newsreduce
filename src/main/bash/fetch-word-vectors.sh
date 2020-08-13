c874b7e0710db754a8dedac65c2c336330eb7394
Seán Healy <sean@seanh.sh>
2020-08-13 20:08:36 +0100

    2020-08-13

--- src/main/bash/fetch-word-vectors.sh
+++ src/main/bash/fetch-word-vectors.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/bash
 # Override the newsreduce dir using --base dir
-base=$(arg base $@)
+base="$(arg base $@)"
 # Pass --fetchonly to skip nodejs processing.
-fetchonly=$(arg onlyfetch $@)
+fetchonly="$(arg onlyfetch $@)"
 if [ ! "$base" ]; then
     base=/var/newsreduce/word-vectors
 fi
 #sources="$(echo 'select u.resource, u.url, label from WordVectorSource s inner join URLView u on u.resource = s.resource' | nr-sql 2>/dev/null)"
-sources=$(cat src/main/bash/word-vector-sources.tsv)
+sources="$(cat src/main/bash/word-vector-sources.tsv)"
 while read source; do
     id="${source%%	*}"
     url="$(echo "$source" | cut -d$'\t' -f2)"
@@ -16,8 +16,9 @@ while read source; do
     mkdir -p $dir
     (cd $dir && wget -nc "$url")
     (cd $dir && find -regex '.*\.zip' -exec unzip -n {} \;)
-    if [ ! "$fetchonly" ]
-    while read path; do
-        node dist/nr-process-word-vectors.js --label="$label" --url="$url" "$path"
-    done <<<$(find "$dir" -regex '.*\.vec')
+    if [ ! "$fetchonly" ]; then
+        while read path; do
+            node dist/nr-process-word-vectors.js --label="$label" --url="$url" "$path"
+        done <<<$(find "$dir" -regex '.*\.vec')
+    fi
 done <<<$(echo "$sources")
